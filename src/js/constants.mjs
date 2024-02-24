c350caaae0dac6d72da5265bdc4228fba3c2cda5
EF-Pami <pamijoelpami@gmail.com>
2024-02-24 15:25:12 +0100

    update constants.mjs

--- src/js/constants.mjs
+++ src/js/constants.mjs
@@ -1,3 +1,5 @@
+import { isUserLoggedIn } from "./api/auth/isUserLoggedIn.mjs";
+
 //API URLs
 export const API_BASE_URL = "https://v2.api.noroff.dev";
 export const API_REGISTER_URL = "/auth/register";
@@ -8,3 +10,17 @@ export const BID_ON_LISTING = "auction/listings/<id>/bids";
 export const ALL_PROFILES = "/auction/profiles";
 export const SINGLE_PROFILE = "auction/profiles/<name>";
 export const ALL_LISTINGS_BY_PROFILES = "/auction/profiles/<name>/listings";
+
+
+//Forms
+export const loginForm = document.querySelector("#loginForm");
+
+//isUserLoggedIn
+export const loggedIn = isUserLoggedIn();
+
+//userInfo
+export const userInfo = storage.load("user");
+
+//API Parameters
+export const profileparams = "?_listings=true";
+export const listingsparams = "?_seller=true&_bids=true&_active=true&sort=created&Order=desc";
