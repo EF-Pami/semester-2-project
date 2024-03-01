d778e71240a21bd482e6e2fd6c2da87379a65a4b
EF-Pami <pamijoelpami@gmail.com>
2024-03-01 22:28:06 +0100

    updated logout button

--- src/js/api/auth/logout.mjs
+++ src/js/api/auth/logout.mjs
@@ -1,12 +1,33 @@
-import * as storage from "../../storage/index.mjs";
-
-/**
- * function clears out the localstorage when user logs out
- */
-
 export function logout() {
-    storage.remove("user");
-    storage.remove("jwt");
-
-    location.href = "../../index.html";
+    const accessToken = localStorage.getItem("accessToken");  
+    if (accessToken) {
+      document.getElementById("logout-btn").classList.remove("d-none");
+    }
+  
+    document.getElementById("logout-btn").addEventListener("click", function () {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("userName");
+      localStorage.removeItem("profile");
+      this.classList.add("d-none");
+      window.location.href = "../index.html";
+    });
+  }
+  
+  export function manageLoginButton() {
+    const accessToken = localStorage.getItem("accessToken");
+    const loginButton = document.getElementById("loginbtn");
+    
+  
+    if (!accessToken) {
+      loginButton.classList.remove("d-none");
+      profile[0].classList.add("d-none");
+    } else if (loginButton) {
+      loginButton.classList.add("d-none");
+    }
+  
+      if (!accessToken && loginButton) {
+      loginButton.addEventListener("click", function () {      
+        window.location.href = "../index.html";
+      });
+    }
 }
